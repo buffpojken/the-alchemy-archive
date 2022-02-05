9bc818baf2ff7fec7568be028b5e613e12af1af8
buffpojken <daniel@sykewarrior.com>
2022-02-05 12:33:08 +0100

    added icons and correct menu labels

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,26 @@ module.exports = {
     './*.html',
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        'louvette': 'louvette-banner, serif'
+      },
+      fontSize: {        
+        '1_5xl': '1.37rem', 
+      },
+      backgroundSize: {
+        'icon-mixturer': '62px 71px',
+        'icon-baser': '76px 73px',
+        'icon-dekokter': '89px 89px',
+        'icon-substantia': '61px 70px'
+      },
+      backgroundImage: theme => ({
+        'icon-mixturer': "url(images/icons/icon-mixturer.svg)",
+        'icon-baser': 'url(images/icons/icon-baser.svg)',
+        'icon-dekokter': 'url(images/icons/icon-dekokter.svg)',
+        'icon-substantia': 'url(images/icons/icon-substantia.svg)',
+      })
+    }
   },
   variants: {},
   plugins: [],
