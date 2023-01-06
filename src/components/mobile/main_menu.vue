4be0ab1cc073095ab2a179767cadef2a7b8f5db2
buffpojken <daniel@sykewarrior.com>
2023-01-06 13:09:32 +0100

    loads of tweaks on mobile menu

--- src/components/mobile/main_menu.vue
+++ src/components/mobile/main_menu.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="py-3 w-screen h-screen box-border transition-transform bg-background">
-    <h3 class="text-white text-center my-6 text-2xl">The Alchemy Archive</h3>
+    <h3 class="text-white text-center my-6 text-4xl font-louvette">The Alchemy Archive</h3>
     <nav class="h-full w-full flex flex-col">
       <ul class="relative h-full mobile-menu-bg">
-        <li class="mobile-menu-item mixtures bg-icon-mixtures" @click.prevent="$router.push('/mixtures')">          
-          <a href="#" class="text-white">Mixtures</a>
+        <li class="mobile-menu-item mixtures" @click.prevent="$router.push('/mixtures')">          
+          <div class="bg-icon-mixtures w-[62px] h-[71px] bg-no-repeat bg-center mt-2 bg-cover"></div>
+          <a href="#" class="text-white mt-2 font-louvette text-xl">Mixtures</a>
         </li>
-        <li class="mobile-menu-item bases bg-icon-bases" @click.prevent="$router.push('/bases')">
-          <a href="#" class="text-white">Bases</a>
+        <li class="mobile-menu-item bases" @click.prevent="$router.push('/bases')">
+          <div class="bg-icon-bases w-[76px] h-[73px] bg-no-repeat bg-center mt-2"></div>
+          <a href="#" class="text-white mt-2 font-louvette text-xl">Bases</a>
         </li>
-        <li class="mobile-menu-item decoctions bg-icon-decoctions" @click.prevent="$router.push('/decoctions')">
-          <a href="#" class="text-white">Dekokter</a>
+        <li class="mobile-menu-item decoctions" @click.prevent="$router.push('/decoctions')">
+          <div class="bg-icon-decoctions w-[89px] h-[89px] bg-no-repeat bg-center mt-2"></div>
+          <a href="#" class="text-white font-louvette text-xl">Dekokter</a>
         </li>
-        <li class="mobile-menu-item substantia bg-icon-substantia" @click.prevent="$router.push('/substantia')">
-          <a href="#" class="text-white">Substantia</a>
+        <li class="mobile-menu-item substantia" @click.prevent="$router.push('/substantia')">
+          <div class="bg-icon-substantia w-[61px] h-[70px] bg-no-repeat bg-center mt-2"></div>
+          <a href="#" class="text-white mt-2 font-louvette text-xl">Substantia</a>
         </li>
       </ul>
     </nav>
