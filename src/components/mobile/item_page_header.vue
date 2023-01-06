d970f7acf31777d1e8e1d54fe2c1c782e54f4853
buffpojken <daniel@sykewarrior.com>
2023-01-06 15:29:52 +0100

    fixed linking issue on mobile item-page-header

--- src/components/mobile/item_page_header.vue
+++ src/components/mobile/item_page_header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="item-page-header flex-none mb-8">
     <div class="main-icon cursor-pointer" @click="$router.push('/mixtures')"></div>
-    <div class="minor-icon"></div>
+    <div class="minor-icon cursor-pointer" @click="$router.push('/')"></div>
     <div class="border-holder"></div>
   </header>
 </template>
