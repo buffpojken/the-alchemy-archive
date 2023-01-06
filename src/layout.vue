f805f5538942fde79b61e9169df3df05d3746f40
buffpojken <daniel@sykewarrior.com>
2023-01-06 11:40:32 +0100

    cleaning up tablet-structure to support tablet-drinks lists

--- src/layout.vue
+++ src/layout.vue
@@ -1,46 +1,19 @@
 <template>
-
-    <MqResponsive target="sm-">
-      <div class="overflow-hidden w-screen max-h-screen flex flex-col">
-        <router-view></router-view>
-      </div>
-    </MqResponsive>
-    <MqResponsive target="md+" class="w-full flex flex-row items-stretch h-full">
-      <a href="why.html" class="the-great-eye">&nbsp;</a>
-
-
-      <div class="stars-container hidden md:flex flex">
-        <div class="stars-inner"></div>
-        <div class="stars-middle rotate-counter-30"></div>
-        <div class="stars-outer  rotate-60" ></div>
-      </div>
-
-      <main class="text-white relative w-full md:w-[550px]">
-        <div class="px-12 mt-12 w-full box-border content-enclosure">
-          <div class="box-border border w-full content-container h-full">
-
-            <div class="content-list-page-decorator-1 move-decorator-1-animate"></div>
-            <div class="content-list-page-decorator-2 move-decorator-2-animate"></div>
-
-            <router-view></router-view>
-
-          </div> <!-- End .content-container -->
-
-        </div>
-      </main>
-
-      <RotaryMenu />
-
-    </MqResponsive>
-
+  <MqResponsive target="sm-">
+    <div class="overflow-hidden w-screen max-h-screen flex flex-col">
+      <router-view></router-view>
+    </div>
+  </MqResponsive>
+  <MqResponsive target="md+" class="w-full flex flex-row items-stretch h-full">
+    <router-view></router-view>
+  </MqResponsive>
 </template>
 
 <script>
   import { MqResponsive } from "vue3-mq";
-  import RotaryMenu from './components/rotary_menu.vue'
 
   export default {
-    components: {MqResponsive, RotaryMenu},
+    components: {MqResponsive},
     data: function(){
       return {
         mobile: true
--- src/sections/ipad_menu_section.vue
+++ src/sections/ipad_menu_section.vue
@@ -1,34 +1,59 @@
 <template>
-  <div class="w-full h-full">
-   <div class="content-list-outer h-full">
-      <div class="content-list-inner h-full">
-        <div class="h-full flex flex-col w-full">
-          <div class="content-list-medalion">
-            <div class="moon-container"></div>
-          </div>
-          <h1 class="content-list-header">Mixturer</h1>
-          <div class="flex-grow w-full overflow-hidden overflow-y-scroll">
-            <ul class="drink-list">
-              <component :is="$route.meta.key" v-for="item in itemList" :item="item" />
+  <a href="why.html" class="the-great-eye">&nbsp;</a>
 
-            </ul>
+  <div class="stars-container hidden md:flex flex">
+    <div class="stars-inner"></div>
+    <div class="stars-middle rotate-counter-30"></div>
+    <div class="stars-outer  rotate-60" ></div>
+  </div>
+
+  <main class="text-white relative w-full md:w-[550px]">
+    <div class="px-12 mt-12 w-full box-border content-enclosure">
+      <div class="box-border border w-full content-container h-full">
+
+        <div class="content-list-page-decorator-1 move-decorator-1-animate"></div>
+        <div class="content-list-page-decorator-2 move-decorator-2-animate"></div>
+
+        <div class="w-full h-full">
+          <div class="content-list-outer h-full">
+            <div class="content-list-inner h-full">
+              <div class="h-full flex flex-col w-full">
+                <div class="content-list-medalion">
+                  <div class="moon-container"></div>
+                </div>
+                <h1 class="content-list-header">Mixturer</h1>
+                <div class="flex-grow w-full overflow-hidden overflow-y-scroll">
+                  <ul class="drink-list">
+                    <component :is="$route.meta.key" v-for="item in itemList" :item="item" />
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </main>
+  <RotaryMenu />
 </template>
 
 <script>
   import {mapState} from 'vuex'
 
+  import RotaryMenu from './../components/rotary_menu.vue'
   import MixtureItem from './../components/list_items/mixture.vue'
   import BaseItem from './../components/list_items/base.vue'
   import DecoctionItem from './../components/list_items/decoction.vue'
   import SubstantiaItem from './../components/list_items/substantia.vue'
 
   export default {
-    components: {'mixtures': MixtureItem, 'bases': BaseItem, 'decoctions': DecoctionItem, 'substantia': SubstantiaItem},
+    components: {
+      'mixtures': MixtureItem, 
+      'bases': BaseItem, 
+      'decoctions': DecoctionItem, 
+      'substantia': SubstantiaItem, 
+      RotaryMenu
+    },
     data: function(){
       return {
 
