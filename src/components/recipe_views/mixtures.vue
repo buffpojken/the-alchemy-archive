28e59dc2c51db105db622c4c114203bcd27893a4
Staffz <s@monkeydancers.com>
2023-03-04 17:22:37 +0100

    Added some styling to mixture page

--- src/components/recipe_views/mixtures.vue
+++ src/components/recipe_views/mixtures.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="overflow-auto h-full mb-6">
-    <ul class="px-6 font-aviano text-lg md:text-2xl mt-6 mb-8">
-      <li v-for="spi in currentItem.spirits">
-        {{spi.amount}} {{spi.name}}
+    <ul class="px-6 font-aviano text-lg md:text-2xl mt-6 mb-8 bg-zinc-900 p-16 ">
+      <li v-for="spi in currentItem.spirits" class="pb-2">
+        <span class="text-2xl">{{spi.amount}}</span> {{spi.name}} <span class="inline-block w-4 h-[21px] ml-1 bg-base-list-icon-filled bg-no-repeat bg-contain">
+    </span> 
       </li>
-      <li v-for="ing in currentItem.ingredients">
+      <li v-for="ing in currentItem.ingredients" class="pb-2">
         {{ing}}
       </li>
     </ul>
 
-    <div class="px-6 font-aviano text-lg md:text-2xl mb-8" v-html="currentItem.description">
+    <div class="px-6 font-aviano text-lg md:text-2xl mb-8 leading-loose" v-html="currentItem.description">
     </div>
   </div>
 </template>
