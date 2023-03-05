1870ffb6e784c021e78cf52bdb64dd0337988e38
Staffz <s@monkeydancers.com>
2023-03-05 20:30:40 +0100

    Started mobile migration to tailwind

--- src/components/mobile/item_page_header.vue
+++ src/components/mobile/item_page_header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="item-page-header flex-none mb-8">
-    <div class="main-icon cursor-pointer" @click="$router.push('/mixtures')">EE</div>
+    <div class=" cursor-pointer absolute rounded-full w-24 h-24 bg-black top-11 left-[calc(50%-50px)] z-20 border-2 border-white" @click="$router.push('/mixtures')">EE</div>
     <div class="minor-icon cursor-pointer" @click="$router.push('/')"></div>
     <div class="border-holder"></div>
   </header>
