6030d7232bb5d99b53137b434ed80f6254ee6935
buffpojken <daniel@sykewarrior.com>
2023-05-16 20:24:58 +0200

    investigating click-issue

--- src/components/list_items/base.vue
+++ src/components/list_items/base.vue
@@ -1,6 +1,6 @@
 <template>
-  <li @click="$router.push(`/bases/`+item.key)" class="font-aviano text-l pb-4 cursor-pointer flex flex-row items-end group">
-    <span class="pt-5 text-center text-xs w-8 h-[42px] mr-3 bg-base-list-icon group-hover:bg-base-list-icon-filled group-hover:text-black  bg-no-repeat bg-contain">
+  <li @click="$router.push(`/bases/`+item.key)" @touchend="$router.push(`/bases/`+item.key)" class="font-aviano text-l pb-4 cursor-pointer flex flex-row items-end group">
+    <span class="pt-5 text-center text-xs w-8 h-[42px] mr-3 bg-base-list-icon group-hover:bg-base-list-icon-filled group-hover:text-black bg-no-repeat bg-contain">
     </span> 
     {{item.name}}
   </li>
