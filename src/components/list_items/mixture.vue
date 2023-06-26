68f58df34543f20ad6ab858b756fad6082dd8ae1
buffpojken <daniel@sykewarrior.com>
2023-06-26 17:23:25 +0200

    fixed touch-issue in menu

--- src/components/list_items/mixture.vue
+++ src/components/list_items/mixture.vue
@@ -1,5 +1,5 @@
 <template>
-  <li @click="$router.push(`/drinks/`+item.slug)" @touchend="$router.push(`/drinks/`+item.slug)" class="font-aviano text-xl pb-4 cursor-pointer flex flex-row items-end group">
+  <li @click="$router.push(`/drinks/`+item.slug)" class="font-aviano text-xl pb-4 cursor-pointer flex flex-row items-end group">
     <span class="pt-5 text-center text-xs w-8 h-[42px] mr-3 bg-mixture-list-icon group-hover:bg-mixture-list-icon-filled group-hover:text-black">
       {{item.objectID}}
     </span> 
